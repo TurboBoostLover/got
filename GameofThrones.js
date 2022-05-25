d79a17325532a0aecbe275eca3f2f28258cd7b15
Nathan Westergard <m800mazdarx8@gmail.com>
2022-05-25 12:54:16 -0600

    Health Kit

--- GameofThrones.js
+++ GameofThrones.js
@@ -1,8 +1,42 @@
+
+//Jon's Stats
 let johnSnowAttack = 25;
+let johnSnowHealth = 100;
+let johnSnowDefense = 0;
+//Jamie's stats
 let jamieLannisterAttack = 35;
 
 if (johnSnowAttack > jamieLannisterAttack) {
     console.log("Jon Snow has better attack than Jamie Lannister");
-} else{
-    console.log("Jamie Lannister has better attack than Jon Snow");
+} else if (johnSnowAttack < jamieLannisterAttack) {
+    console.log("Jamie Lannister has better attack then Jon Snow");
+}else{
+    console.log("Same Attack");
 }
+
+if (johnSnowHealth <= jamieLannisterAttack){
+    console.log("Jon  Snow has been slain");
+} 
+else {
+    //johnSnowHealth = johnSnowHealth - jamieLannisterAttack
+    johnSnowHealth -= jamieLannisterAttack;
+    console.log(`Jon Snow's health is down to ${johnSnowHealth}`);
+}
+//John pick's up a shield
+johnSnowDefense += 25;
+
+if (johnSnowHealth <= jamieLannisterAttack - johnSnowDefense){
+    console.log("Jon  Snow has been slain");
+} 
+else {
+    johnSnowHealth -= (jamieLannisterAttack - johnSnowDefense);
+    console.log(`Jon Snow's health is down to ${johnSnowHealth}`);
+}
+
+//Peasant throws Jon a health kit of 50: heal up!
+if ((johnSnowHealth + 50) >= 100 ) {
+    johnSnowHealth = 100;
+} else {
+    johnSnowHealth += 50;
+}
+
