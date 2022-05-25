413052393c536fe7be805afa323a95bd1991f897
Nathan Westergard <m800mazdarx8@gmail.com>
2022-05-25 12:16:35 -0600

    initial commit: sizing up

---
+++ GameofThrones.js
@@ -0,0 +1,8 @@
+let johnSnowAttack = 25;
+let jamieLannisterAttack = 35;
+
+if (johnSnowAttack > jamieLannisterAttack) {
+    console.log("Jon Snow has better attack than Jamie Lannister");
+} else{
+    console.log("Jamie Lannister has better attack than Jon Snow");
+}
